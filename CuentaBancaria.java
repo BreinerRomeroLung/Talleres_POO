ebd80c78703c3e947eb0d3ada8a110f2d6271cf5
BreinerRomeroLung <breiner.romerolung@unicolombo.edu.co>
2025-02-14 23:16:18 -0500

    CuentaBancaria.java

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -1,20 +1,20 @@
-public class cuentaBancaria {
+public class CuentaBancaria {
     int NumeroCuenta;
     int Saldo;
     String TipoCuenta;
 
-    public cuentaBancaria(){
+    public CuentaBancaria(){
         NumeroCuenta = 23232;
         Saldo =0;
         TipoCuenta="Desconocido";
 
     }
-    public cuentaBancaria(int NumeroCuenta,String TipoCuenta){
+    public CuentaBancaria(int NumeroCuenta,String TipoCuenta){
         this.NumeroCuenta=NumeroCuenta;
         this.TipoCuenta=TipoCuenta;
 
     }
-    public cuentaBancaria(int NumeroCuenta,int Saldo,String TipoCuenta){
+    public CuentaBancaria(int NumeroCuenta,int Saldo,String TipoCuenta){
         this.NumeroCuenta=NumeroCuenta;
         this.Saldo=Saldo;
         this.TipoCuenta=TipoCuenta;
